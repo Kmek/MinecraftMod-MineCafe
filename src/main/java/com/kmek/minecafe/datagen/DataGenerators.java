1aaa67c249216c1664e04a4b9138b4c814fc0841
Kmek <kmekeefe@gmail.com>
2024-01-10 20:22:51 -0500

    Add class for advancements datagen

--- src/main/java/com/kmek/minecafe/datagen/DataGenerators.java
+++ src/main/java/com/kmek/minecafe/datagen/DataGenerators.java
@@ -2,11 +2,15 @@ package com.kmek.minecafe.datagen;
 
 import com.kmek.minecafe.MineCafeMod;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.ForgeAdvancementProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = MineCafeMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
@@ -19,5 +23,18 @@ public class DataGenerators {
         generator.addProvider(true, new ModBlockModelProvider(packOutput, existingFileHelper));
         generator.addProvider(true, new ModBlockStateProvider(packOutput, existingFileHelper));
         generator.addProvider(true, new ModItemModelProvider(packOutput, existingFileHelper));
+
+        // Advancements
+        generator.addProvider(
+                // Tell generator to run only when server data are generating
+                event.includeServer(),
+                (DataProvider.Factory<DataProvider>) output -> new ForgeAdvancementProvider(
+                        output,
+                        event.getLookupProvider(),
+                        event.getExistingFileHelper(),
+                        // Sub providers which generate the advancements
+                        List.of(new ModAdvancementGenerator()/*subProvider1, subProvider2*/)
+                )
+        );
     }
 }
